84f92641803e14b55658f1ea4afeb60203ded758
philipp zupke <trash@bytestyles.net>
2014-11-09 22:33:22 +0100

    Adjusted Y Position for Portal hand

--- Assets/InverseAdjustToHand.cs
+++ Assets/InverseAdjustToHand.cs
@@ -5,22 +5,42 @@ public class InverseAdjustToHand : MonoBehaviour {
 
 	Vector3 RelativHandPosition;
 	public Transform Anchor;
-	void FixedUpdate(){
 
-		if( HandController.Instance.GetAllPhysicsHands().Length <= 	1) return;
+	Vector3 OldLocalAnchorPosition;
 
-		HandModel hand = HandController.Instance.GetAllPhysicsHands()[1];
+	void SetAnchor ()
+	{
+		if (HandController.Instance.GetAllPhysicsHands ().Length <= 1)
+			return;
+		HandModel hand = HandController.Instance.GetAllPhysicsHands () [1];
+		if (hand != null) {
 
-		if(hand != null){
-
-			Vector3 pos = hand.GetWristPosition();
+			Vector3 pos = hand.GetWristPosition ();
 			Vector3 dif = pos - Anchor.position;
-			Debug.DrawLine( Anchor.position, pos, Color.red);
-			Debug.DrawRay( transform.position, dif, Color.cyan);
+			Vector3 zOffset = transform.forward * YAccelleration;
+			Vector3 newAnchorPosition = (transform.position - dif);
+			newAnchorPosition = transform.InverseTransformPoint( newAnchorPosition);
+			newAnchorPosition.z = 0;
+			Anchor.localPosition = newAnchorPosition;
 
-			Anchor.position = transform.position - dif;
+			YAccelleration =  ( OldLocalAnchorPosition.z - Anchor.localPosition.z ) ;
 
+			OldLocalAnchorPosition = Anchor.localPosition;
+
+			Debug.Log(YAccelleration);
+			Debug.DrawLine (Anchor.position, pos, Color.red);
+			Debug.DrawRay (transform.position, dif, Color.cyan);
+			Debug.DrawRay( transform.position, zOffset * 20, Color.yellow);
 		}
+	}
 
+
+	public float HandDistance;
+
+
+	float YAccelleration;
+
+	void LateUpdate(){
+		SetAnchor ();
 	}
 }
